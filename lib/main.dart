739e2965b8a8936f6113a1c6f23b75fa3d647cda
ANDREA_ANGELICA_SANCHEZ_DOMINGUEZ <a.20308051280656@cbtis128.edu.mx>
2023-04-20 14:15:10 +0000

    Actividad 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,121 @@
 import 'package:flutter/material.dart';
+// Uncomment lines 3 and 6 to view the visual layout at runtime.
+// import 'package:flutter/rendering.dart' show debugPaintSizeEnabled;
 
-void main() => runApp(MyApp());
+void main() {
+  // debugPaintSizeEnabled = true;
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(35),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: const Text(
+                    'Serie VNL',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Larga distancia',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          Icon(
+            Icons.bus_alert,
+            color: Colors.red[500],
+          ),
+          const Text('OK'),
+        ],
       ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
-  }
-}
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+    Color color = Theme.of(context).primaryColor;
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    Widget buttonSection = Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        _buildButtonColumn(Color(0xff03325e), Icons.info, 'Informacion'),
+        _buildButtonColumn(Color(0xff03325e), Icons.local_mall, 'Comprar'),
+        _buildButtonColumn(Color(0xff03325e), Icons.save, 'Guardar'),
+        _buildButtonColumn(Color(0xff03325e), Icons.share, 'Compartir'),
+      ],
+    );
+
+    Widget textSection = const Padding(
+      padding: EdgeInsets.all(32),
+      child: Text(
+        'El Volvo VNL se construyó para satisfacer las necesidades de las '
+        'operaciones de transporte a larga distancia de hoy y del futuro.  '
+        'El VNL brinda una eficacia para larga distancia,'
+        ' junto con comodidades prémium.',
+        softWrap: true,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    return MaterialApp(
+      title: 'Camiones Volvo',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter layout demo'),
+          backgroundColor: Color(0xff2b587c),
+        ),
+        body: ListView(
+          children: [
+            Image.asset(
+              'assets/serie1.jpg',
+              width: 600,
+              height: 240,
+              fit: BoxFit.cover,
+            ),
+            titleSection,
+            buttonSection,
+            textSection,
+          ],
         ),
       ),
+    );
+  }
+
+  Column _buildButtonColumn(Color color, IconData icon, String label) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Icon(icon, color: color),
+        Container(
+          margin: const EdgeInsets.only(top: 8),
+          child: Text(
+            label,
+            style: TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w400,
+              color: color,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
